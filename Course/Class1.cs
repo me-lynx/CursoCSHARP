8fe4015319d119ad1e154279c0fee2dc6beea21d
melynxx <maizalouise@gmail.com>
2019-09-03 19:05:58 -0500

    Add files via upload

---
+++ Course/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+    }
+}
